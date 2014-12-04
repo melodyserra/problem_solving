1177ac2d517e555ae2f39a10e8eccb47d1e049f7
Melody Serra <melodyserra@gmail.com>
2014-12-04 12:16:45 -0800

    first commit

--- joshs_problem/solution.rb
+++ joshs_problem/solution.rb
@@ -4,7 +4,7 @@ require 'prime'
 
 while true
   puts "Please enter a string to check if it's Josh's string (all lower case):"
-   
+
   while input_str = gets
     unless input_str =~ /[a-z]+/
       puts "Must be all lower case!"
@@ -19,3 +19,31 @@ while true
     end
   end
 end
+
+# Laura's code:
+# require 'prime'
+# require 'pry'
+
+# def checkstring(string)
+
+#   string = string.downcase
+#   stringarr = string.split("")
+#   alphabet = ('a'...'z').to_a
+#   total = 0
+
+#   stringarr.each do |i|
+#     total += alphabet.index(i).to_i + 1 + total
+
+#   end
+#   if Prime.prime?(total)
+#     puts "This is a prime, word!!!"
+#   else
+#     puts "It isn't a prime, try me again!!!"
+#   end
+
+
+
+# end
+# puts "What's your word?"
+# string = gets.chomp
+# checkstring(string)
